fe0270a106421b11cc3eab894201e38284d2d72c
Gabriel Araujo <gabri1910@outlook.com>
2025-07-25 16:21:31 -0300

    Added a file to calculate the area of a rectangle

---
+++ areaOfRectangle.py
@@ -0,0 +1,9 @@
+# This program uses two users inputs to calculate the area of a rectangle.
+
+print("Hello There!")
+print("Please, input the numbers in meters:")
+
+side = float(input("Whats the measure of the side? "))
+basis = float(input("Whats the measure of the basis? "))
+
+print("Your rectangle has ", side * basis, "meters of area")
